9748eae531d82e8ae2bc98cf5a95996231c67fc1
amitmahajanus <amitmahajanus@gmail.com>
2023-09-19 17:25:09 +0530

    initial files
    adding code

---
+++ src/main/java/com/sc/crmate/samlauth/SamlauthApplication.java
@@ -0,0 +1,13 @@
+package com.sc.crmate.samlauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamlauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamlauthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sc/crmate/samlauth/SamlauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sc.crmate.samlauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamlauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
