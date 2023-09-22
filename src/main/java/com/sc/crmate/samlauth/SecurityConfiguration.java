575a46fcf9aa5e82a8a03c34ec2208e79a455e98
amitmahajanus <amitmahajanus@gmail.com>
2023-09-22 07:38:20 +0530

    adding custom URL's in config

--- src/main/java/com/sc/crmate/samlauth/SecurityConfiguration.java
+++ src/main/java/com/sc/crmate/samlauth/SecurityConfiguration.java
@@ -34,23 +34,62 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfiguration {
 
 	@Bean
-	SecurityFilterChain securityFilterChain(HttpSecurity http,
-			RelyingPartyRegistrationRepository relyingPartyRegistrationRepository) throws Exception {
-		RelyingPartyRegistrationResolver relyingPartyRegistrationResolver = new DefaultRelyingPartyRegistrationResolver(
-				relyingPartyRegistrationRepository);
-		Saml2MetadataFilter metadataFilter = new Saml2MetadataFilter(relyingPartyRegistrationResolver,
-				new OpenSamlMetadataResolver());
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		OpenSaml4AuthenticationProvider authenticationProvider = new OpenSaml4AuthenticationProvider();
+		authenticationProvider.setResponseAuthenticationConverter((responseToken)->{
+			Saml2Authentication authentication = OpenSaml4AuthenticationProvider.createDefaultResponseAuthenticationConverter()
+					.convert(responseToken);
+			return processSaml2Authentication(responseToken,authentication);
+		});
+
 		// @formatter:off
 		http
-			.authorizeHttpRequests((authorize) -> authorize
-				.requestMatchers("/error").permitAll()
-				.anyRequest().authenticated()
-			)
-			.saml2Login(Customizer.withDefaults())
-			.saml2Logout(Customizer.withDefaults())
-			.addFilterBefore(metadataFilter, Saml2WebSsoAuthenticationFilter.class);
+				.authorizeHttpRequests((authorize) -> authorize
+						.requestMatchers("/error").permitAll()
+						.anyRequest().authenticated()
+				)
+				.saml2Login((saml2) -> saml2.relyingPartyRegistrationRepository(relyingPartyRegistrations()))
+				.saml2Logout(Customizer.withDefaults())
+				.saml2Metadata(Customizer.withDefaults());
 		// @formatter:on
 		return http.build();
 	}
 
+	private AbstractAuthenticationToken processSaml2Authentication(OpenSaml4AuthenticationProvider.ResponseToken responseToken, Saml2Authentication authentication) {
+
+		return responseToken.getToken();
+	}
+
+	private RelyingPartyRegistrationRepository relyingPartyRegistrations() {
+		RelyingPartyRegistration registration = RelyingPartyRegistrations.fromMetadataLocation("http://localhost:9090/openam/saml2/jsp/exportmetadata.jsp")
+				.assertingPartyDetails((party)->
+				{
+					party.singleSignOnServiceBinding(Saml2MessageBinding.POST);
+				})
+				.assertionConsumerServiceBinding(Saml2MessageBinding.POST)
+				.registrationId("one")
+				.build();
+		return new InMemoryRelyingPartyRegistrationRepository(registration);
+	}
+
+	@Bean
+	Saml2AuthenticationRequestResolver authenticationRequestResolver(RelyingPartyRegistrationRepository registrations) {
+
+		RelyingPartyRegistration registration = RelyingPartyRegistrations.fromMetadataLocation("http://localhost:9090/openam/saml2/jsp/exportmetadata.jsp")
+				.assertingPartyDetails((party)->
+				{
+					party.singleSignOnServiceBinding(Saml2MessageBinding.POST);
+				})
+				.assertionConsumerServiceBinding(Saml2MessageBinding.POST)
+				.registrationId("one")
+				.build();
+		RelyingPartyRegistrationRepository relyingPartyRegistrationRepository = new InMemoryRelyingPartyRegistrationRepository(registration);
+		OpenSaml4AuthenticationRequestResolver authenticationRequestResolver =
+				new OpenSaml4AuthenticationRequestResolver(relyingPartyRegistrationRepository);
+		authenticationRequestResolver.setAuthnRequestCustomizer((context) -> {
+			context.getAuthnRequest().getIssuer().setValue("https://localhost:8080/newIssuer");
+			context.getAuthnRequest().setAssertionConsumerServiceURL("https://localhost:8080/newACS");
+		});
+		return authenticationRequestResolver;
+	}
 }
